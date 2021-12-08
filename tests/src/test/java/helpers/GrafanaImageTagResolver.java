eb0441442ae11c2685fb795c04fa0b9854d44db7
modem7 <modem7@gmail.com>
2021-12-08 13:54:21 +0000

    deleting tests folder
    Delete build.gradle

--- tests/src/test/java/helpers/GrafanaImageTagResolver.java
+++
@@ -1,9 +0,0 @@
-package helpers;
-
-import io.homecentr.testcontainers.images.EnvironmentImageTagResolver;
-
-public class GrafanaImageTagResolver extends EnvironmentImageTagResolver {
-    public GrafanaImageTagResolver() {
-        super("homecentr/grafana:local");
-    }
-}
